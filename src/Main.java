a14cc9c09022f2dd5600fb0ba3368e0b982d0cda
fadiKhalash <fadikalash358@gmail.com>
2026-06-19 19:29:00 +0300

    تعديل ال main لانو زيد  الهباد رافع المسالة ال2 عليه

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public static void main(String[] args) {
-    SwingUtilities.invokeLater(() -> {
-        TrainNetworkGUI gui = new TrainNetworkGUI();
-        gui.setLocationRelativeTo(null); // فتح النافذة في منتصف الشاشة
-        gui.setVisible(true);
-    });
-}
